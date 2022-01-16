c2a981ebe849d785b2aca0206b9713f3c4c1aa93
drDrozio <ishanss2018@gmail.com>
2022-01-16 14:00:57 +0530

    CRUD API Created

--- src/main/java/com/ishan/sp/topic/TopicController.java
+++ src/main/java/com/ishan/sp/topic/TopicController.java
@@ -1,9 +1,7 @@
 package com.ishan.sp.topic;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,5 +20,20 @@ public class TopicController {
     @RequestMapping("/topics/{id}")
     public Topic getTopic(@PathVariable("id") String id){
         return topicService.getTopic(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value="/topics")
+    public void addTopic(@RequestBody Topic topic){
+        topicService.addTopic(topic);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value="/topics/{id}")
+    public void updateTopic(@RequestBody Topic topic, @PathVariable("id") String id){
+        topicService.updateTopic(id,topic);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value="/topics/{id}")
+    public void deleteTopic(@PathVariable("id") String id){
+        topicService.deleteTopic(id);
     }
 }
--- src/main/java/com/ishan/sp/topic/TopicService.java
+++ src/main/java/com/ishan/sp/topic/TopicService.java
@@ -2,17 +2,18 @@ package com.ishan.sp.topic;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService {
 
-    private List<Topic> topicList = Arrays.asList(
+    private List<Topic> topicList = new ArrayList<>(Arrays.asList(
             new Topic("spring","Spring Framework","Spring Framework Description"),
             new Topic("java","Java","Java Description"),
             new Topic("django","Django Framework","Django Description")
-    );
+    ));
 
     public List<Topic> getAllTopics(){
         return topicList;
@@ -20,5 +21,23 @@ public class TopicService {
 
     public Topic getTopic(String id){
         return topicList.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topicList.add(topic);
+    }
+
+    public void updateTopic(String id, Topic topic) {
+        for(int i=0;i<topicList.size();i++){
+            Topic t = topicList.get(i);
+            if(t.getId().equals(id)){
+                topicList.set(i,topic);
+                return;
+            }
+        }
+    }
+
+    public void deleteTopic(String id) {
+        topicList.removeIf(t -> t.getId().equals(id));
     }
 }
